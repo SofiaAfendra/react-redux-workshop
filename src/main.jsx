67a0dca1d30df4aa19a5c5570e0ba510ada9ce11
OlsiQem <114469211+OlsiQem@users.noreply.github.com>
2023-10-05 14:05:21 +0300

    init commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
-// import * as React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import styles from './index.css'
+import * as React from 'react'
+import ReactDOM from 'react-dom/client'
+import styles from './index.modules.css'
 
-/* TODO: - Create a simple component named "Square" that returns a button, and add a class named 'square'
- *       - Initialize React
+/* TODO: 
+- Change component so when clicked it updates its state and draws 'X' or 'O' alternately
+Hint: A functional component can use multiple state hooks
  */
+
+export const Square = () =>
+   <div className={styles.square}>
+      x
+   </div>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Square />);
