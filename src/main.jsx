0b87a55eb90f3e60bf3af74a047766577324c714
OlsiQem <114469211+OlsiQem@users.noreply.github.com>
2023-11-11 18:03:34 +0200

    react--03: init v2

--- src/main.jsx
+++ src/main.jsx
@@ -9,33 +9,44 @@ import styles from './index.module.css';
 - Modify handleClick so that a square that already has a value does not change.
  */
 
-const Square = () => {
-  const [isNextX, setIsNextX] = useState(true);
-  const [value, setValue] = useState(null);
-
-  const handleClick = () => {
-    isNextX ? setValue('X') : setValue('O');
-    setIsNextX(!isNextX);
-  };
+const Square = ({ value, onClick }) => {
   return (
-    <button onClick={handleClick} className={styles.square}>
+    <button onClick={onClick} className={styles.square}>
       {value}
     </button>
   );
 };
 
 const Board = () => {
+  const [isNextX, setIsNextX] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(''));
 
-  const renderSquare = () => {
-    //Hint - returns the Square Component.
+  const handleClick = (squareIndex) => () => {
+    if (squares[squareIndex]) return;
+    const newSquares = [...squares];
+    newSquares[squareIndex] = isNextX ? 'X' : 'O';
+    setSquares(newSquares);
+    setIsNextX(!isNextX);
   };
 
-  const renderRow = () => {
-    //Hint - returns 3 Squares in a row.
-  };
-  return <div></div>;
+  const renderSquare = (index) => (
+    <Square value={squares[index]} onClick={handleClick(index)} />
+  );
+
+  const renderRow = (index1, index2, index3) => (
+    <div>
+      {renderSquare(index1)}
+      {renderSquare(index2)}
+      {renderSquare(index3)}
+    </div>
+  );
+  return (
+    <div className={styles.board}>
+      {renderRow(0, 1, 2)}
+      {renderRow(3, 4, 5)}
+      {renderRow(6, 7, 8)}
+    </div>
+  );
 };
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Square />);
+ReactDOM.createRoot(document.getElementById('root')).render(<Board />);
