47035ce28ab53132f334f6951f1abdf3b7de2937
OlsiQem <114469211+OlsiQem@users.noreply.github.com>
2023-11-27 02:43:18 +0200

    react--08 make withGameProps

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,77 +1,23 @@
 import { Board } from '../Board';
 import styles from './styles.module.css';
+import { withGameProps } from './withGameProps';
 
-import { useState, useEffect, useRef } from 'react';
-import { config } from '../../config';
-import { calculateWinner, getStatus } from '../../util';
-
-// TO-DO: Use classes from previous state.
-
-export const initialState = {
-  player1: '',
-  player2: '',
-  squares: Array(9).fill(''),
-  isNextX: true,
-  winner: null,
-};
-
-export const Game = () => {
-  const [gameState, setGameState] = useState(
-    JSON.parse(localStorage.getItem(config.PERSIST_KEY)) ?? initialState,
-  );
-  const { player1, player2, squares, isNextX, winner } = gameState;
-  const player1Ref = useRef(null);
-  const player2Ref = useRef(null);
-
-  if (player1Ref.current && winner === 'X') {
-    player1Ref.current.style.border = '2px solid green';
-  }
-
-  if (player2Ref.current && winner === 'O') {
-    player2Ref.current.style.border = '2px solid green';
-  }
-
-  const status = !winner
-    ? `It's ${isNextX ? player1 : player2} 's turn`
-    : getStatus(winner, player1, player2);
-
-  const handleClick = (squareIndex) => () => {
-    if (!(player1 && player2)) {
-      alert('Please, set the names of both players.');
-      return;
-    }
-
-    if (squares[squareIndex]) return;
-
-    const newSquares = [...squares];
-    newSquares[squareIndex] = isNextX ? 'X' : 'O';
-    setGameState({
-      ...gameState,
-      isNextX: !isNextX,
-      squares: newSquares,
-      winner: calculateWinner(newSquares),
-    });
-  };
-
-  const handleReset = () => {
-    if (player1Ref.current) {
-      player1Ref.current.style.border = '';
-    }
-    if (player2Ref.current) {
-      player2Ref.current.style.border = '';
-    }
-
-    setGameState(initialState);
-  };
-
-  useEffect(() => {
-    localStorage.setItem(config.PERSIST_KEY, JSON.stringify(gameState));
-  }, [gameState]);
-
+const GameComponent = ({
+  gameStatus,
+  player1,
+  player1Ref,
+  player2,
+  player2Ref,
+  handleClick,
+  reset,
+  gameState,
+  setGameState,
+  squares,
+}) => {
   return (
     <div className={styles.gameWrapper}>
       <div className={styles.boardAndStatusWrapper}>
-        <div>Game Status: {status}</div>
+        <div>Game Status: {gameStatus}</div>
         <div className={styles.boardWrapper}>
           <Board squares={squares} onClick={handleClick} />
         </div>
@@ -107,7 +53,7 @@ export const Game = () => {
         </div>
 
         <div className={styles.resetBtnWrapper}>
-          <button onClick={handleReset} className={styles.resetButton}>
+          <button onClick={reset} className={styles.resetButton}>
             Reset
           </button>
         </div>
@@ -115,3 +61,5 @@ export const Game = () => {
     </div>
   );
 };
+
+export const Game = withGameProps(GameComponent);
---
+++ src/components/Game/withGameProps.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect, useRef } from 'react';
+import { config } from '../../config';
+import { calculateWinner, getStatus } from '../../util';
+
+// TO-DO: Use classes from previous state.
+
+export const initialState = {
+  player1: '',
+  player2: '',
+  squares: Array(9).fill(''),
+  isNextX: true,
+  winner: null,
+};
+
+export const withGameProps = (WrappedComponent) => (props) => {
+  const [gameState, setGameState] = useState(
+    JSON.parse(localStorage.getItem(config.PERSIST_KEY)) ?? initialState,
+  );
+  const { player1, player2, squares, isNextX, winner } = gameState;
+  const player1Ref = useRef(null);
+  const player2Ref = useRef(null);
+
+  if (player1Ref.current && winner === 'X') {
+    player1Ref.current.style.border = '2px solid green';
+  }
+
+  if (player2Ref.current && winner === 'O') {
+    player2Ref.current.style.border = '2px solid green';
+  }
+
+  const gameStatus = !winner
+    ? `It's ${isNextX ? player1 : player2} 's turn`
+    : getStatus(winner, player1, player2);
+
+  const handleClick = (squareIndex) => () => {
+    if (!(player1 && player2)) {
+      alert('Please, set the names of both players.');
+      return;
+    }
+
+    if (squares[squareIndex]) return;
+
+    const newSquares = [...squares];
+    newSquares[squareIndex] = isNextX ? 'X' : 'O';
+    setGameState({
+      ...gameState,
+      isNextX: !isNextX,
+      squares: newSquares,
+      winner: calculateWinner(newSquares),
+    });
+  };
+
+  const handleReset = () => {
+    if (player1Ref.current) {
+      player1Ref.current.style.border = '';
+    }
+    if (player2Ref.current) {
+      player2Ref.current.style.border = '';
+    }
+
+    setGameState(initialState);
+  };
+
+  useEffect(() => {
+    localStorage.setItem(config.PERSIST_KEY, JSON.stringify(gameState));
+  }, [gameState]);
+
+  return (
+    <WrappedComponent
+      player1={player1}
+      player2={player2}
+      squares={squares}
+      winner={winner}
+      gameStatus={gameStatus}
+      player1Ref={player1Ref}
+      player2Ref={player2Ref}
+      reset={handleReset}
+      handleClick={handleClick}
+      gameState={gameState}
+      setGameState={setGameState}
+      {...props}
+    />
+  );
+};
