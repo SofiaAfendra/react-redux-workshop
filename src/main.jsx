eb0f86c0638580cce115690c439935c8905c8c88
Sofia Afendra <safendra94@gmail.com>
2023-12-17 13:50:00 +0200

    minor change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import styles from './index.module.css';
 /* TODO:  
 - Lift the state from the Square component to the new Board component, and add a class named 'board'.
 - Update renderSquare and renderRow functions to render all the squares needed for the game.
-    hint: Think of each square as an array element that holds a value. 
+  Hint: Think of each square as an array element that holds a value. 
 - Modify handleClick so that a square that already has a value does not change.
  */
 
@@ -18,6 +18,7 @@ const Square = () => {
     xIsNext ? setValue('X') : setValue('O');
     setXIsNext(!xIsNext);
   };
+
   return (
     <button onClick={handleClick} className={styles.square}>
       {value}
