38404f840842ae87c03b263b7b132e6f991ddf9c
Sofia Afendra <safendra94@gmail.com>
2023-12-17 14:49:22 +0200

    minor change

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,5 @@
 // import styles from './index.module.css';
 
 /* TODO: - Create a simple component named "Square" that returns a button, and add a class named 'square'
- *       - Initialize React
- */
+         - Initialize React
+*/
