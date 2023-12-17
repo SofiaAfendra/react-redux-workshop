035de98d7f430f4c592cc989b4e4561241a201d6
Sofia Afendra <safendra94@gmail.com>
2023-12-17 15:09:33 +0200

    minor change

--- src/utils/getStatus.js
+++ src/utils/getStatus.js
@@ -1,14 +1,11 @@
-/*
- * Utility function that take as input the 'winner' and return the status of the game when the game has finished.
- */
+// Utility function that take as input the 'winner' and return the status of the game when the game has finished.
 
 export const getStatus = (winner, player1, player2) => {
-    if (winner === 'X') {
-      return `Winner is: ${player1}`;
-    }
-    if (winner === 'O') {
-      return `Winner is: ${player2}`;
-    }
-    return 'Draw...';
-  };
-  
+  if (winner === 'X') {
+    return `Winner is: ${player1}`;
+  }
+  if (winner === 'O') {
+    return `Winner is: ${player2}`;
+  }
+  return 'Draw...';
+};
