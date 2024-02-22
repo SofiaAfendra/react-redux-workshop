30aec2fba24043b33c503d2ea5bc4a371fb62566
Sofia Afendra <safendra94@gmail.com>
2024-02-22 17:17:31 +0200

    minor change

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -4,15 +4,13 @@ import { connect } from 'react-redux';
 import { squares } from 'models';
 import styles from './styles.module.css';
 
-export const Board = ({ renderRow }) => {
-  return (
-    <div className={styles.board}>
-      {renderRow(0, 1, 2)}
-      {renderRow(3, 4, 5)}
-      {renderRow(6, 7, 8)}
-    </div>
-  );
-};
+export const Board = ({ renderRow }) => (
+  <div className={styles.board}>
+    {renderRow(0, 1, 2)}
+    {renderRow(3, 4, 5)}
+    {renderRow(6, 7, 8)}
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   squares: squares(state),
