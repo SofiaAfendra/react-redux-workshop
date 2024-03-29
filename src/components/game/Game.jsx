afc45231d27d6ba2da10fa5285268139376c6e60
Panagiotis Lamprou <panagiotis.lamprou@newcrosshealthcare.com>
2024-03-29 16:15:57 +0200

    remove squares from Board

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -21,7 +21,6 @@ import styles from './styles.module.css';
 const GameComponent = ({
   player1,
   player2,
-  squares,
   status,
   handleClick,
   handlePlayer1,
@@ -34,7 +33,7 @@ const GameComponent = ({
     <div className={styles.boardAndStatusWrapper}>
       <div>{status}</div>
       <div className={styles.boardWrapper}>
-        <Board squares={squares} onClick={handleClick} />
+        <Board onClick={handleClick} />
       </div>
     </div>
 
