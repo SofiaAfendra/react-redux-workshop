38bdc1f397ba2926fe85fd4afd71ea2ce561a725
OlsiQem <114469211+OlsiQem@users.noreply.github.com>
2024-03-31 06:18:18 +0300

    redux--03 adds todo's

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </UISetup>,
 );
 
+//? rewrite the last bullet
 /* TODO:
-
-- Add a selectors.js in game model folder.
- Create selectors for every key of the state (player1, squares, etc.).
-- At file components/game/Game.jsx:
-  1. rename Game to GameComponent.
-  2. Create a mapStateToProps function to request the required part from state.
-    Hint: use the selectors here.
-  3. Create a mapDispatchToProps, which here is just an object with the required actions.
-  4. Use mapStateToProps and mapDispatchToProps with connect which is a higher order function from Redux.
-  5. Use compose function from Redux to compose both connect and the HoC with the component.
+- Delete the unnecessary files from previous React implementations. (useGameState.js & usePersistState).
+- create persistState.js. In this file create a function that will persist our state to local storage.
+ HINT: it will take on a key parameter and will return an object with a property the initial state that will come from localStorage
+ and the middleware method that will update the state to the localStorage after each action.
+- at store.js make a function makeStore which will run the persistState function and will return the configureStore.
 
 */
