1587fe639081f4c2ec9a5a5c0dfc6f849c2427ee
OlsiQem <114469211+OlsiQem@users.noreply.github.com>
2024-04-03 12:54:04 +0300

    redux--00 minor changes.

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 );
 
 /* TODO:
-- add a folder named store and create stor.js  and configure the redux store of the game.
-- use reducer from the models/test folder
-- Inside the component  folder create a new folder named UI-Setup. Inside that folder create UISetUp.jsx.
+- Add a folder named store and create store.js and configure the redux store of the game.
+- Use reducer from the models/test folder.
+- Inside the component folder create a new folder named UI-Setup. Inside that folder create UISetup.jsx.
 */
