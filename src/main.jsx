8323287e35dc1624a485a9e411d9c5aa7d7f2507
Orestis Marselos <orestis.marselos@sportion.net>
2024-04-15 11:13:51 +0300

    redux-01: updates TODOs

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </UISetup>,
 );
 
-//! rewrite better 3rd bullet
 /* TODO:
 
-- Inside actions.js define and export all tic-tac-toe related actions,
+- Inside actions.js define and export all tic-tac-toe related action functions,
  they should return action objects with the type and payload properties.
-- Inside store/store.js replace initialTestState and testReducer with initialGameState and gameReducer from models/game/reducer.js.
+
 - Inside store folder, create rootReducer.js and define a rootReducer using the reducer.js.
+
+- Inside store/store.js replace initialTestState and testReducer with initialGameState and rootReducer respectively.
+
 - Use Redux Dev Tools to dispatch Custom actions and confirm everything works correctly.
 
 */
