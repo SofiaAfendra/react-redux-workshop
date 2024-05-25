adfb8544b562338f87f0abab1c4110bbf16ce1a9
Orestis Marselos <orestis.marselos@sportion.net>
2024-05-25 15:20:25 +0300

    redux--04: changes selectors to destructure state arg

--- src/models/players/selectors.js
+++ src/models/players/selectors.js
@@ -1,5 +1,4 @@
-const player1 = (state) => state.players.player1;
-
-const player2 = (state) => state.players.player2;
+const player1 = ({ players }) => players.player1;
+const player2 = ({ players }) => players.player2;
 
 export { player1, player2 };
