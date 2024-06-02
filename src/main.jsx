1096e829060d70d4839a2232ad1c33535207393a
OlsiQem <114469211+OlsiQem@users.noreply.github.com>
2024-06-03 00:14:32 +0300

    redux--03--v2 update todo comment.

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </UISetup>,
 );
 
-//? rewrite the last bullet
 /* TODO:
 - Delete the unnecessary files from previous React implementations. (useGameState.js & usePersistState).
-- create persistState.js. In this file create a function that will persist our state to local storage.
+- Create persistState.js. In this file create a function that will persist our state to local storage.
  HINT: it will take on a key parameter and will return an object with a property the initial state that will come from localStorage
  and the middleware method that will update the state to the localStorage after each action.
-- at store.js make a function makeStore which will run the persistState function and will return the configureStore.
+- At store.js make a function "makeStore" which will run the persistState function and will return the configureStore.
 
 */
